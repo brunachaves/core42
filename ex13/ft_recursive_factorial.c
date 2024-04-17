2749797107d1b22a881da1825523121302cc46a4
Bruna Chaves Batista Sousa <brchaves@c3a6c3.42berlin.de>
2024-04-17 16:22:51 +0200

    updated ex02

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brchaves <brchaves@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: brchaves <brchaves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 11:27:14 by brchaves          #+#    #+#             */
-/*   Updated: 2024/04/16 11:31:40 by brchaves         ###   ########.fr       */
+/*   Updated: 2024/04/17 16:21:29 by brchaves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@ int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	else if (nb == 0 || nb == 1)
+	else if (nb == 1 || nb == 0)
 		return (1);
-	else
-		return (nb * ft_recursive_factorial(nb - 1));
+	return (nb * ft_recursive_factorial(nb -1));
 }
